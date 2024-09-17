c9ece2cc75fe0d4963c13fefa072e8d4e274bd18
Santiago Marulanda <santiagomm1997@gmail.com>
2024-09-17 13:18:10 +0200

    Adjust numpad numbers position for custom keymap

--- keyboards/splitkb/aurora/lily58/keymaps/tiagocode/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/tiagocode/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, XXXXXXX, XXXXXXX, KC_MINS, KC_PLUS,  KC_EQL,                    XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX, _______,
         _______, XXXXXXX, XXXXXXX, KC_ASTR, KC_PSLS, KC_PERC,                    XXXXXXX,    KC_4,    KC_5,    KC_6, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, KC_CIRC, KC_COMM,  KC_DOT,  _______, _______, XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,
-                                   _______, XXXXXXX, _______,  _______, _______, _______,    KC_0,  KC_DOT
+                                   _______, XXXXXXX, _______,  _______, _______,    KC_0,  KC_DOT,  _______
     ),
 
     /* 
