bdff2f177fb19e72798c7caff02589d9460cd913
¨Santiago <santiagomm1997@gmail.com>
2024-11-23 01:49:03 +0100

    Fix splitkb/aurora/soflev2 keypmap

--- keyboards/splitkb/aurora/sofle_v2/keymaps/tiagocode/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/tiagocode/keymap.c
@@ -144,8 +144,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_FUNCTIONS] = LAYOUT(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,    KC_9,   KC_10,  KC_F11,  KC_F12, _______,
-    _______, XXXXXXX, KC_LCTL, KC_LALT, KC_LSFT, XXXXXXX,                      XXXXXXX,   KC_F5,   KC_F6,   KC_F7,    KC_8, XXXXXXX,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,
+    _______, XXXXXXX, KC_LCTL, KC_LALT, KC_LSFT, XXXXXXX,                      XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,    _______, XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX,
                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
--- keyboards/splitkb/aurora/sofle_v2/keymaps/tiagocode/rules.mk
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/tiagocode/rules.mk
@@ -1,6 +1,6 @@
 BOOTLOADER = caterina
 # Tap a key and you get its character. Tap a key, but hold it slightly longer and you get its shifted state
-AUTO_SHIFT_ENABLE = yes
+AUTO_SHIFT_ENABLE = no
 # Enables Link Time Optimization (LTO) when compiling the keyboard
 LTO_ENABLE = yes
 # Enables the words per minute (WPM) counter
